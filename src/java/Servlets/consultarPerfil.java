d388e24123e080125419be2c0504d3221894917f
nahuel062 <nambroa@gmail.com>
2018-10-01 16:39:36 -0300

    Merge origin/master
    Conflicts:
    	src/Logica/ctrlUsuario.java
    	src/Persistencia/DBusuario.java

--- src/java/Servlets/consultarPerfil.java
+++ src/java/Servlets/consultarPerfil.java
@@ -45,7 +45,7 @@ public static final String MENSAJE_EXITO = "mensaje_exito";
             throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
        this.IU.cargarUsuarios2();
-   
+
         if (request.getParameter("T") == null) {
                 List<DtUsuario> usuarios = IU.listarUsuarios();
                 request.setAttribute("usuarios", usuarios);
